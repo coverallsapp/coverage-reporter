73a3db25a2b2fdf28ffbcd2b49afba80bbefd0fc
Nick Merwin <n@mer.io>
2020-09-03 13:01:52 -0700

    handle SimpleCov > 0.18 format w/ "lines" vs "branches"

--- src/coverage_reporter/parser_helpers/simplecov.cr
+++ src/coverage_reporter/parser_helpers/simplecov.cr
@@ -3,17 +3,30 @@ module CoverageReporter
     class SimpleCov
       alias SourceFilesType = Hash(Symbol, Array(Int32 | Nil) | String)
 
+      alias CoverageOrHashType = Array(Int32 | Nil) | Hash(String, Array(Int32 | Nil));
+
       def initialize(@tracefile : String)
       end
 
       def parse : Array(SourceFilesType)
         source_files = [] of SourceFilesType
 
-        results = Hash(String, Hash(String, Hash(String, Array(Int32 | Nil)) | Int32)).from_json File.read @tracefile
+        # handle SimpleCov versions before 0.18 that don't include branch coverage
+        results = Hash(String, Hash(String, Hash(String, CoverageOrHashType) | Int32)).from_json File.read @tracefile
 
         results.each do |test_runner, output|
-          # Need to cast again to prevent the "timestamp": Int32 field from erroring:
-          output["coverage"].as(Hash(String, Array(Int32 | Nil))).each do |filename, coverage|
+          output["coverage"].as(Hash(String, CoverageOrHashType)).each do |filename, coverage_or_hash|
+
+            coverage = [] of Int32 | Nil
+
+            case coverage_or_hash
+            when Array(Int32 | Nil)
+              coverage = coverage_or_hash
+            when Hash(String, Array(Int32 | Nil))
+              coverage = coverage_or_hash["lines"]
+              # TODO: handle coverage_or_hash["branches"]
+            end
+
             source_files.push({
               :name => filename.sub(Dir.current,""), 
               :coverage => coverage
