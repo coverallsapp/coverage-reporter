6518174f9dcf889c49e2822ad2c8d2d2359566ed
Lee Donahoe <leedonahoe@gmail.com>
2020-09-05 14:13:44 -0700

    added silly console printing

--- src/cli.cr
+++ src/cli.cr
@@ -1,5 +1,6 @@
 require "option_parser"
 require "./coverage_reporter"
+require "colorize"
 
 filename = ""
 repo_token = ENV.fetch("COVERALLS_REPO_TOKEN", "")
@@ -40,14 +41,21 @@ parser = OptionParser.parse do |parser|
 end
 
 begin
-  puts <<-'STR'
-     _____                         _ _
-    / ____|                       | | |
-   | |     _____   _____ _ __ __ _| | |___
-   | |    / _ \ \ / / _ \ '__/ _` | | / __|
-   | |___| (_) \ V /  __/ | | (_| | | \__ \
-    \_____\___/ \_/ \___|_|  \__,_|_|_|___/
-  STR
+  puts "                                       #{"j".colorize(Colorize::Color256.new(88))}#{"i".colorize(Colorize::Color256.new(196))}#{"y".colorize(Colorize::Color256.new(88))}"
+  puts "                                      #{"f".colorize(Colorize::Color256.new(88))}#{"lfl".colorize(Colorize::Color256.new(196))}#{"v".colorize(Colorize::Color256.new(88))}"
+  puts "                                     #{"p".colorize(Colorize::Color256.new(88))}#{"wwwww".colorize(Colorize::Color256.new(196))}#{"q".colorize(Colorize::Color256.new(88))}"
+  puts "                               #{"l".colorize(Colorize::Color256.new(88))}#{"jtttttwwwwwtttttj".colorize(Colorize::Color256.new(196))}#{"l".colorize(Colorize::Color256.new(88))}"
+  puts "                                  #{"p".colorize(Colorize::Color256.new(88))}#{"jwwwwwwwwwj".colorize(Colorize::Color256.new(196))}#{"q".colorize(Colorize::Color256.new(88))}"
+  puts "                                    #{"m".colorize(Colorize::Color256.new(88))}#{"wwwewww".colorize(Colorize::Color256.new(196))}#{"k".colorize(Colorize::Color256.new(88))}"
+  puts "                                  #{"n".colorize(Colorize::Color256.new(88))}#{"jwwj".colorize(Colorize::Color256.new(196))}#{"v".colorize(Colorize::Color256.new(88))} #{"v".colorize(Colorize::Color256.new(88))}#{"jwwy".colorize(Colorize::Color256.new(196))}#{"n".colorize(Colorize::Color256.new(88))}"
+  puts "                                 #{"m".colorize(Colorize::Color256.new(52))}#{"jy".colorize(Colorize::Color256.new(196))}#{"q".colorize(Colorize::Color256.new(88))}       #{"p".colorize(Colorize::Color256.new(88))}#{"gj".colorize(Colorize::Color256.new(196))}#{"m".colorize(Colorize::Color256.new(52))}"
+  puts " "
+  puts "        #{"g".colorize(Colorize::Color256.new(240))}vwww#{"y".colorize(Colorize::Color256.new(240))}  #{"v".colorize(Colorize::Color256.new(240))}www#{"g".colorize(Colorize::Color256.new(240))}  #{";".colorize(Colorize::Color256.new(240))}ww  www :wwww  vwnwng     nwv    ywp   ywp    ywww"
+  puts "      #{"f".colorize(Colorize::Color256.new(240))}nwn#{"*".colorize(Colorize::Color256.new(240))}''  ww#{"n".colorize(Colorize::Color256.new(240))} ww: #{".".colorize(Colorize::Color256.new(240))}ww  ww  wwr   #{":".colorize(Colorize::Color256.new(240))}fw vww#{"v".colorize(Colorize::Color256.new(240))}  #{"f".colorize(Colorize::Color256.new(240))}wnmw#{"n".colorize(Colorize::Color256.new(240))}   ww#{";".colorize(Colorize::Color256.new(240))}   ww#{";".colorize(Colorize::Color256.new(240))}   ww#{"*".colorize(Colorize::Color256.new(240))}''"
+  puts "     #{"v".colorize(Colorize::Color256.new(240))}fww     ww#{"j".colorize(Colorize::Color256.new(240))}  ww#{"g".colorize(Colorize::Color256.new(240))} #{".".colorize(Colorize::Color256.new(240))}ww ww  :wwww  fwm#{"e".colorize(Colorize::Color256.new(240))}ny   #{"f".colorize(Colorize::Color256.new(240))}ww #{";".colorize(Colorize::Color256.new(240))}ww  #{"f".colorize(Colorize::Color256.new(240))}ww   #{"f".colorize(Colorize::Color256.new(240))}ww    www:"
+  puts "     #{"n".colorize(Colorize::Color256.new(240))}www#{"v".colorize(Colorize::Color256.new(240))}    ww  #{"v".colorize(Colorize::Color256.new(240))}ww#{"y".colorize(Colorize::Color256.new(240))}  ww#{":".colorize(Colorize::Color256.new(240))}wf  www    www'ww   wwwoowwi fww   fww     :ww;"
+  puts "      #{"m".colorize(Colorize::Color256.new(240))}wwww#{"l".colorize(Colorize::Color256.new(240))}  #{"v".colorize(Colorize::Color256.new(240))}wwwon#{"n".colorize(Colorize::Color256.new(240))}   wwwf  :wwwww #{"v".colorize(Colorize::Color256.new(240))}ww  wwi lwwi  wwl lwwww lwwww iswj#{"v".colorize(Colorize::Color256.new(240))}"
+  puts " "
   puts "  v#{CoverageReporter::VERSION}\n\n"
 
   CoverageReporter.run(filename, repo_token, config_path, job_flag)
