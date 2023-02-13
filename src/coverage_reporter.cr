79bc8e2767cdfa51aacf99904c45ea18c452d349
Valentin Kiselev <mrexox@evilmartians.com>
2023-02-13 17:08:30 +0300

    docs: update documentation and comments

--- src/coverage_reporter.cr
+++ src/coverage_reporter.cr
@@ -7,6 +7,9 @@ module CoverageReporter
 
   # Parses the coverage reports in the current directory or the given *coverage_file*
   # and sends the report to Coveralls API.
+  #
+  # If *coverage_file* is provided only its content will be parsed. Otherwise
+  # current directory will be searched for all supported report formats.
   def report(
     coverage_file : String?,
     base_path : String?,
@@ -27,6 +30,12 @@ module CoverageReporter
     api.send_request(dry_run)
   end
 
+  # Reports that all parallel jobs were reported and Coveralls can aggregate
+  # coverage results.
+  #
+  # Refers to a build via `service_number` parameter which is either taken
+  # from a CI-specific ENV, or can be set explicitly via `COVERALLS_SERVICE_NUMBER`
+  # environment variable.
   def parallel_done(repo_token : String?, config_path : String, dry_run : Bool)
     config = Config.new(repo_token: repo_token, path: config_path)
     api = Api::Webhook.new(config)
