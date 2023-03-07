98fa67c33e749ee42b3786b8331b9f16dd46e809
Valentin Kiselev <mrexox@evilmartians.com>
2023-03-07 14:09:13 +0300

    0.2.7: add jacoco format support

--- src/coverage_reporter.cr
+++ src/coverage_reporter.cr
@@ -3,7 +3,7 @@ require "./coverage_reporter/*"
 module CoverageReporter
   extend self
 
-  VERSION = "0.2.6"
+  VERSION = "0.2.7"
 
   # Parses the coverage reports in the current directory or the given *coverage_file*
   # and sends the report to Coveralls API.
