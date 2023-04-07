6a8339c006fda4fc54292a72de845c1d7cd5ec4d
Valentin Kiselev <mrexox@evilmartians.com>
2023-04-07 11:14:20 +0300

    fix: remove service_job_number, add service_number, pull_request

--- src/coverage_reporter/ci/gitlab.cr
+++ src/coverage_reporter/ci/gitlab.cr
@@ -10,11 +10,12 @@ module CoverageReporter
 
         Options.new(
           service_name: "gitlab-ci",
+          service_number: ENV["CI_PIPELINE_IID"]?,
+          service_build_url: ENV["CI_PIPELINE_URL"]? || ENV["CI_JOB_URL"]?,
           service_job_id: ENV["CI_JOB_NAME"]?,
-          service_job_number: ENV["CI_JOB_ID"]?,
-          service_branch: ENV["CI_COMMIT_REF_NAME"]?,
-          service_build_url: ENV["CI_JOB_URL"]?,
           service_job_url: ENV["CI_JOB_URL"]?,
+          service_pull_request: ENV["CI_MERGE_REQUEST_IID"]?,
+          service_branch: ENV["CI_COMMIT_REF_NAME"]?,
           commit_sha: ENV["CI_COMMIT_SHA"]?,
         ).to_h
       end
