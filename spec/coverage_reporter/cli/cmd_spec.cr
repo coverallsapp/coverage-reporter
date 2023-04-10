c20c00bb6700dcdbd49a0990c445278ed1099696
Valentin Kiselev <mrexox@evilmartians.com>
2023-04-10 11:54:24 +0300

    chore: add tests on CLI options

--- spec/coverage_reporter/cli/cmd_spec.cr
+++ spec/coverage_reporter/cli/cmd_spec.cr
@@ -11,6 +11,14 @@ Spectator.describe CoverageReporter::Cli do
   after_each { ENV.clear }
 
   describe ".run" do
+    it "applies defaults" do
+      reporter = subject.run %w(--dry-run)
+
+      expect(reporter.fail_empty).to eq true
+      expect(reporter.dry_run).to eq true
+      expect(reporter.parallel).to eq false
+    end
+
     it "parses overrides" do
       reporter = subject.run %w(--service-name overriden --dry-run --no-logo)
 
@@ -34,6 +42,42 @@ Spectator.describe CoverageReporter::Cli do
 
       expect(reporter.dry_run).to eq true
       expect(reporter.overrides.try(&.to_h)).to eq({} of Symbol => String)
+    end
+
+    it "accepts --allow-empty option" do
+      reporter = subject.run %w(
+        --allow-empty
+        --dry-run
+      )
+
+      expect(reporter.fail_empty).to eq false
+    end
+
+    it "accepts --carryforward option" do
+      reporter = subject.run %w(
+        --carryforward "1,2,3"
+        --dry-run
+      )
+
+      expect(reporter.carryforward).to eq "\"1,2,3\""
+    end
+
+    it "accepts --format option" do
+      reporter = subject.run %w(
+        --format lcov
+        --dry-run
+      )
+
+      expect(reporter.coverage_format).to eq "lcov"
+    end
+
+    it "accepts --filename option" do
+      reporter = subject.run %w(
+        --file spec/fixtures/lcov/test.lcov
+        --dry-run
+      )
+
+      expect(reporter.coverage_file).to eq "spec/fixtures/lcov/test.lcov"
     end
   end
 end
