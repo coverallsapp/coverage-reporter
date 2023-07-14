c4f2af4b279b35766af012f981638ee28f728bff
Valentin Kiselev <mrexox@evilmartians.com>
2023-07-14 12:05:02 +0300

    chore: add tests

--- spec/coverage_reporter/cli/cmd_spec.cr
+++ spec/coverage_reporter/cli/cmd_spec.cr
@@ -163,6 +163,7 @@ Spectator.describe CoverageReporter::Cli do
           -m
           --build-number 3
           --carryforward 1,2,3
+          --create-build
           --dry-run
         )
       end
@@ -170,6 +171,27 @@ Spectator.describe CoverageReporter::Cli do
       it "accepts --carryforward option" do
         expect(subject).to eq 0
         expect(reporter.carryforward).to eq "1,2,3"
+        expect(reporter.create_build).to eq true
+        expect(reporter.overrides.try(&.to_h)).to eq({
+          :service_number => "3",
+        })
+      end
+    end
+
+    context "without carryforward (new args)" do
+      let(options) do
+        %w(
+          done
+          -m
+          --build-number 3
+          --no-fail
+        )
+      end
+
+      it "doesn't fail" do
+        expect(subject).to eq 0
+        expect(reporter.carryforward).to eq nil
+        expect(reporter.create_build).to eq false
         expect(reporter.overrides.try(&.to_h)).to eq({
           :service_number => "3",
         })
