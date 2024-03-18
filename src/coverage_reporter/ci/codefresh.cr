82f9ab10758c136d1d590afdd0f9fc34acd61aa9
snackattas <zach.attas@gmail.com>
2024-03-18 09:23:41 -0500

    fix Codefresh PR number variable

--- src/coverage_reporter/ci/codefresh.cr
+++ src/coverage_reporter/ci/codefresh.cr
@@ -11,7 +11,7 @@ module CoverageReporter
         Options.new(
           service_name: "Codefresh",
           service_job_id: ENV["CF_BUILD_ID"]?,
-          service_pull_request: ENV["CF_PULL_REQUEST_ID"]?,
+          service_pull_request: ENV["CF_PULL_REQUEST_NUMBER"]?,
           service_branch: ENV["CF_BRANCH"]?,
           commit_sha: ENV["CF_REVISION"]?,
         ).to_h
