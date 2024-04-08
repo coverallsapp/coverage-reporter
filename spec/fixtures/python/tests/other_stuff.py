ac469acf9bbc239ef70900d87bdef7689a5dd488
Jeanine Soterwood <jeanine@thoughtbot.com>
2024-04-08 11:35:23 -0700

    Update coverage.py parser (#116)
    Replace the existing method of parsing the SQLite
    .coverage file in favor of using the Coverage.py
    XML reporting to get an XML file that can be parsed
    with the existing CoberturaParser.
    
    As this change to the parser requires the existing
    ENV["PATH"] variable to be set, many of the specs
    had to be modified so that they did not clear the
    entire environment.
    
    This also updates the spectator shard version.
    
    ---------
    
    Co-authored-by: Mike Burns <mburns@thoughtbot.com>

--- spec/fixtures/python/tests/other_stuff.py
+++
@@ -1,6 +0,0 @@
-def test_in_non_test_module():
-    """
-    PyTest will recognize this function as a test...
-    But will not collect tests from this file (by default)
-    """
-    print("\nRunning test_in_non_test_module...")
--- src/coverage_reporter/parsers/coveragepy_parser.cr
+++ src/coverage_reporter/parsers/coveragepy_parser.cr
@@ -3,15 +3,12 @@ require "sqlite3"
 
 module CoverageReporter
   class CoveragepyParser < BaseParser
+    class ParserError < RuntimeError
+    end
+
     def self.name
       "python"
     end
-
-    QUERY = <<-SQL
-    SELECT file.path, line_bits.numbits
-      FROM line_bits
-    INNER JOIN file ON (line_bits.file_id = file.id)
-    SQL
 
     def globs : Array(String)
       [
@@ -30,114 +27,25 @@ module CoverageReporter
       false
     end
 
-    def parse(filename : String) : Array(FileReport)
-      lines = {} of String => Array(Hits)
+    def parse(filename : String, error : Process::Stdio = IO::Memory.new) : Array(FileReport)
+      tmpfile = File.tempfile("coverage.xml")
+      process_status = Process.run(
+        command: "coverage xml --data-file #{filename} -o #{tmpfile.path}",
+        shell: true,
+        error: error
+      )
 
-      DB.open "sqlite3://#{filename}" do |db|
-        db.query(QUERY) do |rs|
-          rs.each do
-            name = rs.read(String)
-            numbits = rs.read(Slice(UInt8))
-            nums = [] of Hits
-            numbits.each_with_index do |byte, byte_i|
-              8.times do |bit_i|
-                if byte & (1 << bit_i) != 0
-                  nums << (byte_i * 8 + bit_i).to_u64
-                end
-              end
-            end
-            lines[name] = nums
-          end
-        end
+      if process_status.success?
+        parser = CoberturaParser.new(@base_path)
+        parser.parse(tmpfile.path)
+      else
+        raise ParserError.new(error.to_s)
       end
-
-      lines.map do |name, hits|
-        coverage = get_coverage(name, hits)
-
-        file_report(
-          name: name,
-          coverage: coverage,
-        )
+    ensure
+      begin
+        tmpfile && tmpfile.delete
+      rescue File::Error
       end
-    end
-
-    private def get_coverage(name : String, hits : Array(Hits)) : Array(Hits?)
-      coverage = {} of Line => Hits?
-
-      line_no = 1.to_u64
-      under_def = false
-      docstring = false
-      brackets = 0
-
-      File.each_line(name, chomp: true) do |line|
-        code = line.strip
-
-        if code.ends_with?(/\(|\{|\[/)
-          brackets += code.count("({[")
-          brackets -= code.count(")}]")
-        end
-
-        if !docstring && code.starts_with?("\"\"\"")
-          if under_def || hits.find { |i| i == line_no }
-            docstring = true
-            coverage[line_no] = nil
-            next
-          end
-        end
-
-        # docstring
-        if docstring
-          if code.ends_with?("\"\"\"")
-            docstring = false
-          end
-
-          coverage[line_no] = nil
-          next
-        end
-
-        # comment
-        if code.starts_with?("#")
-          coverage[line_no] = nil
-          next
-        end
-
-        # a hit
-        if hits.find { |i| i == line_no }
-          coverage[line_no] = 1
-          next
-        end
-
-        # inside brackets
-        if brackets > 0
-          coverage[line_no] = nil
-          next
-        end
-
-        # empty string
-        if code.empty?
-          coverage[line_no] = nil
-          next
-        end
-
-        coverage[line_no] = 0
-      ensure
-        line_no += 1
-
-        if code
-          if brackets > 0 && code.ends_with?(/\)|\}|\]/)
-            brackets += code.count("({[")
-            brackets -= code.count(")}]")
-          end
-
-          under_def = code.starts_with?("def ") || code.starts_with?("class ")
-        end
-      end
-
-      coverage.keys.sort!.map { |k| coverage[k] }
-    rescue File::NotFoundError
-      Log.error("Couldn't open file #{name}")
-
-      [] of Hits?
     end
   end
 end
