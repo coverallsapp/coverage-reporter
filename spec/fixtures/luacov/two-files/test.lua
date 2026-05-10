c52bbf8eac53ac3d627559d282f3bd5b7e9dd10c
luau-project <luau.project@gmail.com>
2026-05-09 23:31:35 -0300

    spec(fixtures): add LuaCov files

---
+++ spec/fixtures/luacov/two-files/test.lua
@@ -0,0 +1,19 @@
+-- hello world
+if true then
+    a = 2
+else
+    c = 1
+end
+for i = 1, 10 do
+    if i % 7 == 0 then
+        require("my-lib")()
+    else
+        print(0)
+    end
+end
+if a == 2 then
+    b = 3
+else
+    c = 2
+end
+--
